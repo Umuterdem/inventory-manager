d41fb92e857d32519452eae16f7873cea08e2bd0
Abdulkerim Atik <abdulkerim.atik@atez.com.tr>
2021-08-11 13:46:22 +0300

    initial commit

---
+++ src/test/java/com/atez/internship/example/internshipexample/InternshipExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atez.internship.example.internshipexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
